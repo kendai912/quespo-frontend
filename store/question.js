cbdcdafa633d56b122065a99764d4d492bc72076
Kendai <kendai912@gmail.com>
2021-12-01 22:08:56 +0900

    fix:API更新を反映

--- store/question.js
+++ store/question.js
@@ -8,14 +8,22 @@ import {
 } from "./util";
 
 const state = () => ({
+  status: null,
   result: null,
 });
 
 const getters = {
+  status: (state) => state.status,
   result: (state) => state.result,
 };
 
 const mutations = {
+  setStatus(state, data) {
+    state.status = data;
+  },
+  clearStatus(state) {
+    state.status = null;
+  },
   setResult(state, data) {
     state.result = data;
   },
@@ -25,16 +33,30 @@ const mutations = {
 };
 
 const actions = {
-  async getResult(context, optionId) {
+  async showQuestion(context, question_id) {
+    const response = await this.$axios
+      .get("/api/questions/" + question_id)
+      .catch((err) => err.response || err);
+
+    if (response.status == OK) {
+      context.commit("setStatus", response.data.question.status);
+    } else if (response.status === UNPROCESSABLE_ENTITY) {
+      console.log("UNPROCESSABLE_ENTITY");
+    } else {
+      console.log(response.status);
+    }
+  },
+  async getResult(context, input) {
     let params = {
-      optionId: optionId,
+      question_id: input.question_id,
+      option_id: input.option_id,
     };
 
     const response = await this.$axios
       .post("/api/question/answer", params)
       .catch((err) => err.response || err);
 
-    if (response.status == OK) {
+    if (response.status == CREATED) {
       context.commit("setResult", response.data.result);
     } else if (response.status === UNPROCESSABLE_ENTITY) {
       console.log("UNPROCESSABLE_ENTITY");
